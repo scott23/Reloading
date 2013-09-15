4793e53fc78a2ae4452f569c6f0cc3ba8e1ac126
Scott Stiffler <scotts@rainks.com>
2013-09-15 13:46:36 -0500

    Updates

--- Test/scripts/app.js
+++ Test/scripts/app.js
@@ -1,36 +1,10 @@
 (function (global) {
-    var mobileSkin = "",
-        app = global.app = global.app || {};
+    app = global.app = global.app || {};
 
     document.addEventListener("deviceready", function () {
         app.application = new kendo.mobile.Application(document.body, { 
             layout: "tabstrip-layout",
-        	transition:'zoom'
+        	/*transition:'zoom'*/
         });
-    
     }, false);
-
-    app.changeSkin = function (e) {
-        if (e.sender.element.text() === "Flat") {
-            e.sender.element.text("Native");
-            mobileSkin = "flat";
-        }
-        else {
-            e.sender.element.text("Flat");
-            mobileSkin = "";
-        }
-
-        app.application.skin(mobileSkin);
-    };
-    
-})(window)
-
-
-    function compute() {
-          var a = $('#a').val();
-          var b = $('#b').val();
-          var total = a * b;
-          $('#total').val(total);
-        }
-
-        $('#a, #b').keyup(compute);
+})(window);
---
+++ Test/scripts/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+				  function compute() {
+                      var caseTotal = $('#casePrice').val() / $('#caseQty').val() || 0;
+                      var primerTotal = $('#primerPrice').val() / $('#primerQty').val() || 0;
+                      var powderTotal = $('#powderPrice').val() / ($('#powderQty').val() * 7000 / $('#grainsQty').val()) || 0;
+                      var bulletTotal = $('#bulletPrice').val() / $('#bulletQty').val() || 0;
+                      var total = (caseTotal + primerTotal + powderTotal + bulletTotal);
+                      var finalTotal = (total.toFixed(3));
+                      
+                      var rounds = $('#rounds').val();
+                      $('#roundsPrice').val(rounds * finalTotal);
+                      $('#onePrice').val(1 * finalTotal);
+                      $('#50Price').val(50 * finalTotal);
+                      $('#100Price').val(100 * finalTotal);
+                      $('#500Price').val(500 * finalTotal);
+                      $('#1000Price').val(1000 * finalTotal);
+                      
+                      console.log(caseTotal, primerTotal, powderTotal, bulletTotal, rounds, finalTotal);
+                  }
+                    $('.field').keyup(compute);
+
+});
